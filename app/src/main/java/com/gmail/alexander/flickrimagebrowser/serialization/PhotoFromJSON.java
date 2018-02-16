8e02d1a67c4a6856c693578fd9db085aa69e830e
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-02-16 18:01:39 +0200

    PhotoFromJSON completed.

--- app/src/main/java/com/gmail/alexander/flickrimagebrowser/serialization/PhotoFromJSON.java
+++ app/src/main/java/com/gmail/alexander/flickrimagebrowser/serialization/PhotoFromJSON.java
@@ -9,6 +9,11 @@ import com.gmail.alexander.flickrimagebrowser.datadownloader.OnDataAvailable;
 import com.gmail.alexander.flickrimagebrowser.datadownloader.OnDownloadComplete;
 import com.gmail.alexander.flickrimagebrowser.models.Photo;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -36,22 +41,49 @@ public class PhotoFromJSON implements OnDownloadComplete {
 
     @Override
     public void onDownloadComplete(String data, DownloadStatus downloadStatus) {
+        if (downloadStatus == DownloadStatus.OK) {
+            photos = new ArrayList<>();
+            try {
+                JSONObject jsonData = new JSONObject(data);
+                JSONArray itemsArray = jsonData.getJSONArray("items");
+                for (int i = 0; i < itemsArray.length(); i++) {
+                    JSONObject jsonPhoto = itemsArray.getJSONObject(i);
+                    String title = jsonPhoto.getString("title");
+                    String author = jsonPhoto.getString("author");
+                    String authorId = jsonPhoto.getString("author_id");
+                    String tags = jsonPhoto.getString("tags");
 
+                    JSONObject jsonMedia = jsonPhoto.getJSONObject("media");
+                    String photoUrl = jsonMedia.getString("m");
+                    String link = photoUrl.replaceFirst("_m.", "_b.");
+                    Photo photo = new Photo(title, author, authorId, link, tags, photoUrl);
+                    photos.add(photo);
+                    Log.d(TAG, "onDownloadComplete: " + photo.toString());
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                downloadStatus = DownloadStatus.FAIL_OR_EMPTY;
+            }
+            if (callBack != null) {
+                callBack.onDataAvailable(photos, downloadStatus);
+            }
+        }
     }
-    void executeOnSameThread(String searchCriteria){
+
+    public void executeOnSameThread(String searchCriteria) {
         Log.d(TAG, "executeOnSameThread: Starts");
-        String destinationUri = createUri(searchCriteria,language,matchAll);
+        String destinationUri = createUri(searchCriteria, language, matchAll);
         GetRawData getRawData = new GetRawData(this);
         getRawData.execute(destinationUri);
         Log.d(TAG, "executeOnSameThread: ends");
     }
 
     private String createUri(String searchCriteria, String language, boolean matchAll) {
-        return Uri.parse(baseUrl).buildUpon().appendQueryParameter("tags",searchCriteria)
-                .appendQueryParameter("tagmode",matchAll ? "ALL" : "ANY")
-                .appendQueryParameter("lang",language)
-                .appendQueryParameter("format","json")
-                .appendQueryParameter("nojsoncallback","1")
+        return Uri.parse(baseUrl).buildUpon().appendQueryParameter("tags", searchCriteria)
+                .appendQueryParameter("tagmode", matchAll ? "ALL" : "ANY")
+                .appendQueryParameter("lang", language)
+                .appendQueryParameter("format", "json")
+                .appendQueryParameter("nojsoncallback", "1")
                 .build().toString();
     }
 }
