1fc12553eb99b4d344da02f0d20d73d1b6aae7e8
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-02-16 16:02:10 +0200

    Working on createUri method.

--- app/src/main/java/com/gmail/alexander/flickrimagebrowser/serialization/PhotoFromJSON.java
+++ app/src/main/java/com/gmail/alexander/flickrimagebrowser/serialization/PhotoFromJSON.java
@@ -1,6 +1,9 @@
 package com.gmail.alexander.flickrimagebrowser.serialization;
 
+import android.util.Log;
+
 import com.gmail.alexander.flickrimagebrowser.datadownloader.DownloadStatus;
+import com.gmail.alexander.flickrimagebrowser.datadownloader.GetRawData;
 import com.gmail.alexander.flickrimagebrowser.datadownloader.OnDataAvailable;
 import com.gmail.alexander.flickrimagebrowser.datadownloader.OnDownloadComplete;
 import com.gmail.alexander.flickrimagebrowser.models.Photo;
@@ -15,13 +18,34 @@ import java.util.List;
  */
 
 public class PhotoFromJSON implements OnDownloadComplete {
+    private static final String TAG = "PhotoFromJSON";
     private List<Photo> photos = null;
     private String baseUrl;
     private String language;
     private boolean matchAll;
     private final OnDataAvailable callBack;
+
+    public PhotoFromJSON(String baseUrl, String language, boolean matchAll, OnDataAvailable callBack) {
+        Log.d(TAG, "PhotoFromJSON: called");
+        this.baseUrl = baseUrl;
+        this.language = language;
+        this.matchAll = matchAll;
+        this.callBack = callBack;
+    }
+
     @Override
     public void onDownloadComplete(String data, DownloadStatus downloadStatus) {
 
+    }
+    void executeOnSameThread(String searchCriteria){
+        Log.d(TAG, "executeOnSameThread: Starts");
+        String destinationUri = createUri(searchCriteria,language,matchAll);
+        GetRawData getRawData = new GetRawData(this);
+        getRawData.execute(destinationUri);
+        Log.d(TAG, "executeOnSameThread: ends");
+    }
+
+    private String createUri(String searchCriteria, String language, boolean matchAll) {
+        return null;
     }
 }
