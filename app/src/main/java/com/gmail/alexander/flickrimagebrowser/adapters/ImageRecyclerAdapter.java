c2c591b55ed253e142d56046a62e9f4449620d18
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-02-28 14:46:26 +0200

    Working on adapter for image display. Working on adapter for image display# Please enter the commit message for your changes. Lines starting

---
+++ app/src/main/java/com/gmail/alexander/flickrimagebrowser/adapters/ImageRecyclerAdapter.java
@@ -0,0 +1,11 @@
+package com.gmail.alexander.flickrimagebrowser.adapters;
+
+/**
+ * Created by:
+ *
+ * @author Alexander Vladimirov
+ *         <alexandervladimirov1902@gmail.com>
+ */
+
+public class ImageRecyclerAdapter {
+}
