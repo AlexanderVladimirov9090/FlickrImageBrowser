1ff5aa33f59e9e788a6772f36f08bfe42a695838
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-02-28 13:50:38 +0200

    Adding new functionality for searching images.

--- app/src/main/java/com/gmail/alexander/flickrimagebrowser/activities/MainActivity.java
+++ app/src/main/java/com/gmail/alexander/flickrimagebrowser/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.gmail.alexander.flickrimagebrowser;
+package com.gmail.alexander.flickrimagebrowser.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
