a102ec682dd8f6be7f127bf943ea42a08e8523e5
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-03-04 12:36:01 +0200

    Implimenting methods of the Adapter.

--- app/src/main/java/com/gmail/alexander/flickrimagebrowser/holders/ImageViewHolder.java
+++ app/src/main/java/com/gmail/alexander/flickrimagebrowser/holders/ImageViewHolder.java
@@ -1,5 +1,13 @@
 package com.gmail.alexander.flickrimagebrowser.holders;
 
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.gmail.alexander.flickrimagebrowser.R;
+
 /**
  * Created by:
  *
@@ -7,5 +15,15 @@ package com.gmail.alexander.flickrimagebrowser.holders;
  *         <alexandervladimirov1902@gmail.com>
  */
 
-public class ImageViewHolder {
+public class ImageViewHolder extends RecyclerView.ViewHolder {
+    private static final String TAG = "ImageViewHolder";
+    ImageView thumbnail = null;
+    TextView title = null;
+
+    public ImageViewHolder(View itemView) {
+        super(itemView);
+        Log.d(TAG, "ImageViewHolder: Constructor Called");
+        this.thumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
+        this.title = (TextView) itemView.findViewById(R.id.photoTitle);
+    }
 }
