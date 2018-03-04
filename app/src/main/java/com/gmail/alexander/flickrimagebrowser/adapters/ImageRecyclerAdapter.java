9f1cbc9c80d90a10c875465632f272019faebd2d
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-03-04 12:36:01 +0200

    Implimenting methods of the Adapter.

--- app/src/main/java/com/gmail/alexander/flickrimagebrowser/adapters/ImageRecyclerAdapter.java
+++ app/src/main/java/com/gmail/alexander/flickrimagebrowser/adapters/ImageRecyclerAdapter.java
@@ -2,6 +2,7 @@ package com.gmail.alexander.flickrimagebrowser.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
 import com.gmail.alexander.flickrimagebrowser.holders.ImageViewHolder;
 import com.gmail.alexander.flickrimagebrowser.models.Photo;
@@ -20,4 +21,23 @@ public class ImageRecyclerAdapter extends RecyclerView.Adapter<ImageViewHolder>{
     private List<Photo> photos;
     private Context context;
 
+    public ImageRecyclerAdapter(List<Photo> photos, Context context) {
+        this.photos = photos;
+        this.context = context;
+    }
+
+    @Override
+    public ImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ImageViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
