a808351ed672bd8fd728528546f85f947e4adca1
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-03-04 11:54:10 +0200

    Working on image adapter.

--- app/src/main/java/com/gmail/alexander/flickrimagebrowser/adapters/ImageRecyclerAdapter.java
+++ app/src/main/java/com/gmail/alexander/flickrimagebrowser/adapters/ImageRecyclerAdapter.java
@@ -1,5 +1,13 @@
 package com.gmail.alexander.flickrimagebrowser.adapters;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+import com.gmail.alexander.flickrimagebrowser.holders.ImageViewHolder;
+import com.gmail.alexander.flickrimagebrowser.models.Photo;
+
+import java.util.List;
+
 /**
  * Created by:
  *
@@ -7,5 +15,9 @@ package com.gmail.alexander.flickrimagebrowser.adapters;
  *         <alexandervladimirov1902@gmail.com>
  */
 
-public class ImageRecyclerAdapter {
+public class ImageRecyclerAdapter extends RecyclerView.Adapter<ImageViewHolder>{
+    private static final String TAG = "ImageRecyclerAdapter";
+    private List<Photo> photos;
+    private Context context;
+
 }
