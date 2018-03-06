60eeaa7c548d942d0a89804e83e7378612c6aaa4
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-03-06 12:59:23 +0200

    Extracting logic to BaseActivity.

---
+++ app/src/main/java/com/gmail/alexander/flickrimagebrowser/activities/BaseActivity.java
@@ -0,0 +1,41 @@
+package com.gmail.alexander.flickrimagebrowser.activities;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.gmail.alexander.flickrimagebrowser.R;
+
+/**
+ * Created by:
+ *
+ * @author Alexander Vladimirov
+ *         <alexandervladimirov1902@gmail.com>
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    private static final String TAG = "BaseActivity";
+    static final String FLICKR_QUERY = "FLICKER_QUERY";
+    static final String PHOTO_TRANSFER = "PHOTO_TRANSFER";
+
+    /**
+     *
+     * @param enableHome
+     */
+    public void activateToolbar(boolean enableHome) {
+        Log.d(TAG, "activateToolbar: Starts");
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+            if (toolbar != null) {
+                setSupportActionBar(toolbar);
+                actionBar = getSupportActionBar();
+            }
+        }
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(enableHome);
+        }
+    }
+}
