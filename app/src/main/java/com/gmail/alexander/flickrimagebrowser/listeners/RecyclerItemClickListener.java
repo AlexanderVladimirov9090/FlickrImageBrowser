f225578fe0951a9b7388bc7c43780f1456a7376c
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-03-06 12:40:06 +0200

    Listener up but still work to do.

--- app/src/main/java/com/gmail/alexander/flickrimagebrowser/listeners/RecyclerItemClickListener.java
+++ app/src/main/java/com/gmail/alexander/flickrimagebrowser/listeners/RecyclerItemClickListener.java
@@ -5,6 +5,7 @@ import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by:
@@ -19,17 +20,26 @@ public class RecyclerItemClickListener extends RecyclerView.SimpleOnItemTouchLis
     private final OnRecyclerClickListener onRecyclerClickListener;
     private final GestureDetectorCompat gestureDetectorCompat;
 
-    public RecyclerItemClickListener(Context context, OnRecyclerClickListener onRecyclerClickListener, RecyclerView recyclerView) {
+    public RecyclerItemClickListener(Context context, final OnRecyclerClickListener onRecyclerClickListener, final RecyclerView recyclerView) {
         this.onRecyclerClickListener = onRecyclerClickListener;
-        gestureDetectorCompat = new GestureDetectorCompat(context, new GestureDetector.SimpleOnGestureListener(){
+        gestureDetectorCompat = new GestureDetectorCompat(context, new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onSingleTapUp(MotionEvent e) {
-                                return super.onSingleTapUp(e);
+                View childView = recyclerView.findChildViewUnder(e.getX(), e.getY());
+
+                if (childView != null && onRecyclerClickListener != null) {
+                    onRecyclerClickListener.onItemClick(childView, recyclerView.getChildAdapterPosition(childView));
+                }
+                return true;
             }
 
             @Override
             public void onLongPress(MotionEvent e) {
-                super.onLongPress(e);
+                View childView = recyclerView.findChildViewUnder(e.getX(), e.getY());
+
+                if (childView != null && onRecyclerClickListener != null) {
+                    onRecyclerClickListener.onItemClick(childView, recyclerView.getChildAdapterPosition(childView));
+                }
             }
         });
 
