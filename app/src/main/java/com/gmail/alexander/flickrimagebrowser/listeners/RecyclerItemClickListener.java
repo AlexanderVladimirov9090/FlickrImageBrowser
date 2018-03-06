d7f294ed99005095148ca61586ae8eca9af55143
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-03-06 12:17:24 +0200

    Working on listeners.

--- app/src/main/java/com/gmail/alexander/flickrimagebrowser/listeners/RecyclerItemClickListener.java
+++ app/src/main/java/com/gmail/alexander/flickrimagebrowser/listeners/RecyclerItemClickListener.java
@@ -1,19 +1,43 @@
 package com.gmail.alexander.flickrimagebrowser.listeners;
 
+import android.content.Context;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 /**
  * Created by:
  *
  * @author Alexander Vladimirov
  *         <alexandervladimirov1902@gmail.com>
- * Handles onclick events.
+ *         Handles onclick events.
  */
 
 public class RecyclerItemClickListener extends RecyclerView.SimpleOnItemTouchListener {
     private static final String TAG = "RecyclerItemClick";
     private final OnRecyclerClickListener onRecyclerClickListener;
     private final GestureDetectorCompat gestureDetectorCompat;
+
+    public RecyclerItemClickListener(Context context, OnRecyclerClickListener onRecyclerClickListener, RecyclerView recyclerView) {
+        this.onRecyclerClickListener = onRecyclerClickListener;
+        gestureDetectorCompat = new GestureDetectorCompat(context, new GestureDetector.SimpleOnGestureListener(){
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                                return super.onSingleTapUp(e);
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                super.onLongPress(e);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        return gestureDetectorCompat.onTouchEvent(e);
+    }
 
 }
