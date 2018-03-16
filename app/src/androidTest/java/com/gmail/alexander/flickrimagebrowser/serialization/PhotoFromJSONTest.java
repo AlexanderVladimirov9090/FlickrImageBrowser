e61cd8c18d332b9afd796f51608cfea025938c7b
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-03-16 11:40:27 +0200

    Writing tests.

---
+++ app/src/androidTest/java/com/gmail/alexander/flickrimagebrowser/serialization/PhotoFromJSONTest.java
@@ -0,0 +1,11 @@
+package com.gmail.alexander.flickrimagebrowser.serialization;
+
+/**
+ * Created by:
+ *
+ * @author Alexander Vladimirov
+ *         <alexandervladimirov1902@gmail.com>
+ */
+public class PhotoFromJSONTest {
+
+}
