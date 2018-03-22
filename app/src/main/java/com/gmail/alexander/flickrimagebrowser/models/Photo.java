27ce43de88c2a6499e9f949551787bba2801e5e3
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-03-22 12:04:22 +0200

    Writing some test.

--- app/src/main/java/com/gmail/alexander/flickrimagebrowser/models/Photo.java
+++ app/src/main/java/com/gmail/alexander/flickrimagebrowser/models/Photo.java
@@ -66,4 +66,31 @@ public class Photo implements Serializable {
                 ", tags='" + tags + '\'' +
                 ", image='" + image + '\'';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Photo photo = (Photo) o;
+
+        if (title != null ? !title.equals(photo.title) : photo.title != null) return false;
+        if (author != null ? !author.equals(photo.author) : photo.author != null) return false;
+        if (authorId != null ? !authorId.equals(photo.authorId) : photo.authorId != null)
+            return false;
+        if (link != null ? !link.equals(photo.link) : photo.link != null) return false;
+        if (tags != null ? !tags.equals(photo.tags) : photo.tags != null) return false;
+        return image != null ? image.equals(photo.image) : photo.image == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + (authorId != null ? authorId.hashCode() : 0);
+        result = 31 * result + (link != null ? link.hashCode() : 0);
+        result = 31 * result + (tags != null ? tags.hashCode() : 0);
+        result = 31 * result + (image != null ? image.hashCode() : 0);
+        return result;
+    }
 }
